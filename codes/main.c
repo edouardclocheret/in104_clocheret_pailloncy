2196aa0901ffd89bf697757fbdfe414159df31ef
edouardclocheret <edouard.clocheret@ensta-paris.fr>
2023-04-11 17:36:34 +0200

    Essai d'affichage de fenêtre

--- codes/main.c
+++ codes/main.c
@@ -16,21 +16,29 @@ int main(int argc, char *argv[]){
     
     //Création et ouverture de la fenêtre
     
-    SDL_Window *window = NULL;
+SDL_Window *window = NULL, *window2 = NULL;
     if(0 != SDL_Init(SDL_INIT_VIDEO))
     {
         fprintf(stderr, "Erreur SDL_Init : %s", SDL_GetError());
         return EXIT_FAILURE;
     }
-    window = SDL_CreateWindow("SDL2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+    window = SDL_CreateWindow("SDL2", 100, 100,
                               640, 480, SDL_WINDOW_SHOWN);
     if(NULL == window)
     {
         fprintf(stderr, "Erreur SDL_CreateWindow : %s", SDL_GetError());
         return EXIT_FAILURE;
     }
+    window2 = SDL_CreateWindow("SDL2", 700, 500,
+                               640, 480, SDL_WINDOW_SHOWN);
+    if(NULL == window2)
+    {
+        fprintf(stderr, "Erreur SDL_CreateWindow 2 : %s", SDL_GetError());
+        return EXIT_FAILURE;
+    }
     SDL_Delay(3000);
     SDL_DestroyWindow(window);
+    SDL_DestroyWindow(window2);
     SDL_Quit();
 
     return EXIT_SUCCESS;
