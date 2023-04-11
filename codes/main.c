5765c843a5088abe569cdcb37a9e28c06421ffc5
edouardclocheret <edouard.clocheret@ensta-paris.fr>
2023-04-11 17:11:08 +0200

    ajout de l'ouverture de fenetres

--- codes/main.c
+++ codes/main.c
@@ -2,14 +2,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
+    
+    
     printf("running\n");
+
+    //Initialisation de SDL
     if(0 != SDL_Init(SDL_INIT_VIDEO))
     {
         fprintf(stderr, "Erreur SDL_Init : %s", SDL_GetError());
         return EXIT_FAILURE;
     }
+    
+    //Création et ouverture de la fenêtre
+    
+    SDL_Window *window = NULL;
+    if(0 != SDL_Init(SDL_INIT_VIDEO))
+    {
+        fprintf(stderr, "Erreur SDL_Init : %s", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+    window = SDL_CreateWindow("SDL2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                              640, 480, SDL_WINDOW_SHOWN);
+    if(NULL == window)
+    {
+        fprintf(stderr, "Erreur SDL_CreateWindow : %s", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+    SDL_Delay(3000);
+    SDL_DestroyWindow(window);
     SDL_Quit();
+
     return EXIT_SUCCESS;
 }
