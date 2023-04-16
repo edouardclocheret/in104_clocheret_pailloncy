4e40b1ee61f908b66d8ce8adbb1cbe4fba0205ef
edouardclocheret <edouard.clocheret@ensta-paris.fr>
2023-04-16 19:57:20 +0200

    ajout de la création de rendus

--- codes/main.c
+++ codes/main.c
@@ -8,7 +8,8 @@ int main(int argc, char *argv[]){
     printf("running\n");
 
     //Initialisation de SDL
-    if(0 != SDL_Init(SDL_INIT_VIDEO))
+    
+    if(SDL_Init(SDL_INIT_VIDEO) <0)
     {
         fprintf(stderr, "Erreur SDL_Init : %s", SDL_GetError());
         return EXIT_FAILURE;
@@ -17,28 +18,31 @@ int main(int argc, char *argv[]){
     //Création et ouverture de la fenêtre
     
 SDL_Window *window = NULL, *window2 = NULL;
-    if(0 != SDL_Init(SDL_INIT_VIDEO))
-    {
-        fprintf(stderr, "Erreur SDL_Init : %s", SDL_GetError());
-        return EXIT_FAILURE;
-    }
-    window = SDL_CreateWindow("SDL2", 100, 100,
-                              640, 480, SDL_WINDOW_SHOWN);
+    
+    window = SDL_CreateWindow("SDL2", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
     if(NULL == window)
     {
         fprintf(stderr, "Erreur SDL_CreateWindow : %s", SDL_GetError());
         return EXIT_FAILURE;
     }
-    window2 = SDL_CreateWindow("SDL2", 700, 500,
-                               640, 480, SDL_WINDOW_SHOWN);
-    if(NULL == window2)
-    {
-        fprintf(stderr, "Erreur SDL_CreateWindow 2 : %s", SDL_GetError());
-        return EXIT_FAILURE;
+    
+    //Création du rendu
+    SDL_Renderer* pRenderer;
+    pRenderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);       
+    if (pRenderer == NULL)     
+    {         
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "[DEBUG] > %s", SDL_GetError());         
+        SDL_Quit();         
+        return EXIT_FAILURE;     
     }
+
     SDL_Delay(3000);
     SDL_DestroyWindow(window);
-    SDL_DestroyWindow(window2);
+
+    if(NULL != pRenderer) SDL_DestroyRenderer(pRenderer);
+    if(NULL != window) SDL_DestroyWindow(window);
+    SDL_Quit();
+    
     SDL_Quit();
 
     return EXIT_SUCCESS;
