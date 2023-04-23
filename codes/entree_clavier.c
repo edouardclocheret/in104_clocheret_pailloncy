b61de88bd3395352279fc902bb7817b3d1b055d6
pailloncy <louispailloncy@macbook-pro-de-louis-2.home>
2023-04-23 12:30:00 +0200

    3 entrees claviers non bloquantes : 2 rotations et 1 chute de bloc

--- codes/entree_clavier.c
+++ codes/entree_clavier.c
@@ -1,3 +1,25 @@
+#include "allinclude.h"
+
+//entree clavier non bloquante 
 //touche Q pour tourner le bloc vers la gauche 
 //touche D pour tourner le bloc vers la droite 
 //touche S pour faire descendre le bloc
+
+while (run) {
+    while (SDL_PollEvent(&events)) {
+        switch(events.type) {
+        case SDL_KEYDOWN:
+
+            if (events.key.keysym.sym == SDLK_Q)
+                rotation(-1);
+            if (events.key.keysym.sym == SDLK_S)
+                down(); //chute du bluc, à programmer 
+            if (events.key.keysym.sym == SDLK_D)
+                rotation(1);
+
+            break;
+        }
+    }
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+}
