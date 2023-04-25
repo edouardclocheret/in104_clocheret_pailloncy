6406ec8adcaf00c9e067d3143987a9f47eca2186
pailloncy <louispailloncy@MacBook-Pro-de-Louis-2.local>
2023-04-25 15:53:15 +0200

    programmation de la rotation

--- codes/rotation.c
+++ codes/rotation.c
@@ -3,12 +3,11 @@
 // argument -1 pour une rotation vers la gauche
 //argument +1 pour une rotation vers la droite
 
-bloc rotation (int sens){
+void rotation (int sens, bloc* falling_meteor){
     if (sens == -1){
-
+        falling_meteor->rotation=(falling_meteor->rotation-1)%4;
     }
-
     if (sens == 1){
-        
+        falling_meteor->rotation=(falling_meteor->rotation+1)%4;
     }
 }
