9cb9d4723ba1e4ad523bab2e793bad8269b2f700
edouardclocheret <edouard.clocheret@ensta-paris.fr>
2023-04-25 15:37:05 +0200

    définition claire de ce qu est un bloc

--- codes/def_types_et_vars.h
+++ codes/def_types_et_vars.h
@@ -2,10 +2,15 @@
 #define taille_carreau 50
 #define dy 50
 
-enum nom = {I, O, T, L, J, Z, S};
+//A name is given to each tetris bloc
+enum nom {I, O, T, L, J, Z, S};
 
+//Refer to bloc_names.png to see every bloc in its default/reference configuration
 typedef struct bloc {
-    enum nom;
+    enum nom son_nom;
+    //position of the most right and down sub-bloc (including empty spaces)
     int x;
     int y;
+    //number of clockwise rotations from the default configuration
+    int rotation;
 } bloc ;
