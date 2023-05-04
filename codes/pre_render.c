14523410d5649b076d3da0132c64a314b211fc95
edouardclocheret <edouard.clocheret@ensta-paris.fr>
2023-05-04 17:29:25 +0200

    corr

--- codes/pre_render.c
+++ codes/pre_render.c
@@ -10,7 +10,10 @@ void pre_render(SDL_Renderer* pRenderer, bloc * falling_meteor){
             // Création du rectangle
             SDL_Rect rectangle;
             
-            if (falling_meteor->rotation==0||falling_meteor->rotation==2){
+            if (falling_meteor->rotation==0){
+                SDL_Rect rectangle = {0,0,50,50};
+            }
+            if (/*falling_meteor->rotation==0||*/falling_meteor->rotation==2){
                 SDL_Rect rectangle = { falling_meteor->x-4*taille_carreau, 
                                         falling_meteor->y-taille_carreau, 
                                         4*taille_carreau, taille_carreau };
