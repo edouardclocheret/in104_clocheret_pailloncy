0f49917e9db0271af04fac4b2e8651230345163d
pailloncy <louispailloncy@MacBook-Pro-de-Louis-2.local>
2023-05-05 10:59:34 +0200

    blocs 0 et T

--- codes/pre_render.c
+++ codes/pre_render.c
@@ -5,7 +5,7 @@
 void pre_render(SDL_Renderer* pRenderer, bloc * falling_meteor){
     switch(falling_meteor->son_nom){
         case I :
-            // Définition de la couleur du rectangle
+            // Définition de la couleur du rectangle (turquoise)
             SDL_SetRenderDrawColor(pRenderer, 255, 0, 255, 255);
             // Création du rectangle
             SDL_Rect rectangle;
@@ -28,8 +28,37 @@ void pre_render(SDL_Renderer* pRenderer, bloc * falling_meteor){
             SDL_RenderPresent(pRenderer);
             break;
         case O:
+            // Définition de la couleur du rectangle (jaune)
+            SDL_SetRenderDrawColor(pRenderer, 255, 255, 0, 255);
+            // Création du rectangle
+            SDL_Rect rectangle;
+            SDL_Rect rectangle = { falling_meteor->x-2*taille_carreau, 
+                                        falling_meteor->y-2*taille_carreau, 
+                                        2*taille_carreau, 2*taille_carreau };
+            // Affichage du rectangle
+            SDL_RenderFillRect(pRenderer, &rectangle);
+
+            // Mise à jour du rendu
+            SDL_RenderPresent(pRenderer);
             break;
         case T:
+            // Définition de la couleur du rectangle (violet)
+            SDL_SetRenderDrawColor(pRenderer, 255, 0, 255, 255);
+            // Création du rectangle
+            SDL_Rect rectangle;
+            if (falling_meteor->rotation==0||falling_meteor->rotation==2){
+                SDL_Rect rectangle = { falling_meteor->x-3*taille_carreau, 
+                                        falling_meteor->y-2*taille_carreau, 
+                                        3*taille_carreau, 2*taille_carreau };}
+            if (falling_meteor->rotation==1||falling_meteor->rotation==3){
+                SDL_Rect rectangle = { falling_meteor->x-3*taille_carreau, 
+                                        falling_meteor->y-2*taille_carreau, 
+                                        3*taille_carreau, 2*taille_carreau };}
+            // Affichage du rectangle
+            SDL_RenderFillRect(pRenderer, &rectangle);
+
+            // Mise à jour du rendu
+            SDL_RenderPresent(pRenderer);
             break;
         case L:
             break;
