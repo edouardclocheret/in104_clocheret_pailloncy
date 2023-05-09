a1d57d217da22b84a7fd74953b814f4e3d68bab1
pailloncy <louispailloncy@MacBook-Pro-de-Louis-2.local>
2023-05-09 15:50:13 +0200

    avancees

--- codes/pre_render.c
+++ codes/pre_render.c
@@ -9,17 +9,14 @@ void pre_render(SDL_Renderer** pRenderer, bloc * falling_meteor){
             // Définition de la couleur du rectangle (turquoise)
             SDL_SetRenderDrawColor(*pRenderer, 255, 0, 255, 255);
 
-
+            rectangle.x = falling_meteor->x;
+            rectangle.y = falling_meteor->y;
             if (falling_meteor->rotation==0||falling_meteor->rotation==2){
-                rectangle.x = falling_meteor->x;
-                rectangle.y = falling_meteor->y;
                 rectangle.w = 4*taille_carreau;
                 rectangle.h = taille_carreau;
 
             }
             if (falling_meteor->rotation==1||falling_meteor->rotation==3){
-                rectangle.x = falling_meteor->x;
-                rectangle.y = falling_meteor->y;
                 rectangle.w = taille_carreau;
                 rectangle.h =4*taille_carreau;
     
@@ -29,84 +26,215 @@ void pre_render(SDL_Renderer** pRenderer, bloc * falling_meteor){
             SDL_RenderFillRect(*pRenderer, &rectangle);
 
             break;
-            
+
         case O:
             // Définition de la couleur du rectangle (jaune)
             SDL_SetRenderDrawColor(*pRenderer, 255, 255, 0, 255);
-
-            rectangle = { falling_meteor->x-2*taille_carreau, 
-                                        falling_meteor->y-2*taille_carreau, 
-                                        2*taille_carreau, 2*taille_carreau };
+            rectangle.x = falling_meteor->x;
+            rectangle.y = falling_meteor->y;
+            rectangle.w = 2*taille_carreau;
+            rectangle.h =2*taille_carreau;
+    
             // Affichage du rectangle
             SDL_RenderFillRect(*pRenderer, &rectangle);
 
-            
             break;
+
         case T:
             // Définition de la couleur du rectangle (violet)
             SDL_SetRenderDrawColor(*pRenderer, 255, 0, 255, 255);
-            
-            if (falling_meteor->rotation==0||falling_meteor->rotation==2){
-                rectangle = { falling_meteor->x-3*taille_carreau, 
-                                        falling_meteor->y-2*taille_carreau, 
-                                        3*taille_carreau, 2*taille_carreau };}
-            if (falling_meteor->rotation==1||falling_meteor->rotation==3){
-                rectangle = { falling_meteor->x-2*taille_carreau, 
-                                        falling_meteor->y-3*taille_carreau, 
-                                        3*taille_carreau, 2*taille_carreau };}
+
+            SDL_Rect rectangle2;
+
+            if (falling_meteor->rotation==0){
+                rectangle.x = falling_meteor->x;
+                rectangle.y = falling_meteor->y;
+                rectangle.w = 3*taille_carreau;
+                rectangle.h = taille_carreau;
+                rectangle2.x = falling_meteor->x+1;
+                rectangle2.y = falling_meteor->y+1;
+                rectangle2.w = taille_carreau;
+                rectangle2.h = taille_carreau;
+            }
+            if (falling_meteor->rotation==2){
+                rectangle.x = falling_meteor->x;
+                rectangle.y = falling_meteor->y+1;
+                rectangle.w = 3*taille_carreau;
+                rectangle.h = taille_carreau;
+                rectangle2.x = falling_meteor->x+1;
+                rectangle2.y = falling_meteor->y;
+                rectangle2.w = taille_carreau;
+                rectangle2.h = taille_carreau;
+            }
+            if (falling_meteor->rotation==1){
+                rectangle.x = falling_meteor->x+1;
+                rectangle.y = falling_meteor->y;
+                rectangle.w = taille_carreau;
+                rectangle.h = 3*taille_carreau;
+                rectangle2.x = falling_meteor->x;
+                rectangle2.y = falling_meteor->y+1;
+                rectangle2.w = taille_carreau;
+                rectangle2.h = taille_carreau;
+            }
+            if (falling_meteor->rotation==3){
+                rectangle.x = falling_meteor->x;
+                rectangle.y = falling_meteor->y;
+                rectangle.w = taille_carreau;
+                rectangle.h = 3*taille_carreau;
+                rectangle2.x = falling_meteor->x+1;
+                rectangle2.y = falling_meteor->y+1;
+                rectangle2.w = taille_carreau;
+                rectangle2.h = taille_carreau;
+            }
+
             // Affichage du rectangle
             SDL_RenderFillRect(*pRenderer, &rectangle);
 
             break;
+
         case L:
              // Définition de la couleur du rectangle (orange)
             SDL_SetRenderDrawColor(*pRenderer, 255, 128, 0, 255);
             
-            if (falling_meteor->rotation==0||falling_meteor->rotation==2){
-                rectangle = { falling_meteor->x-3*taille_carreau, 
-                                        falling_meteor->y-2*taille_carreau, 
-                                        3*taille_carreau, 2*taille_carreau };}
-            if (falling_meteor->rotation==1||falling_meteor->rotation==3){
-                rectangle = { falling_meteor->x-2*taille_carreau, 
-                                        falling_meteor->y-3*taille_carreau, 
-                                        3*taille_carreau, 2*taille_carreau };}
+            if (falling_meteor->rotation==0){
+                rectangle.x = falling_meteor->x;
+                rectangle.y = falling_meteor->y;
+                rectangle.w = 3*taille_carreau;
+                rectangle.h = taille_carreau;
+                rectangle2.x = falling_meteor->x;
+                rectangle2.y = falling_meteor->y+1;
+                rectangle2.w = taille_carreau;
+                rectangle2.h = taille_carreau;
+            }
+            if (falling_meteor->rotation==2){
+                rectangle.x = falling_meteor->x;
+                rectangle.y = falling_meteor->y+1;
+                rectangle.w = 3*taille_carreau;
+                rectangle.h = taille_carreau;
+                rectangle2.x = falling_meteor->x+2;
+                rectangle2.y = falling_meteor->y;
+                rectangle2.w = taille_carreau;
+                rectangle2.h = taille_carreau;
+            }
+            if (falling_meteor->rotation==1){
+                rectangle.x = falling_meteor->x+1;
+                rectangle.y = falling_meteor->y;
+                rectangle.w = taille_carreau;
+                rectangle.h = 3*taille_carreau;
+                rectangle2.x = falling_meteor->x;
+                rectangle2.y = falling_meteor->y;
+                rectangle2.w = taille_carreau;
+                rectangle2.h = taille_carreau;
+            }
+            if (falling_meteor->rotation==3){
+                rectangle.x = falling_meteor->x;
+                rectangle.y = falling_meteor->y;
+                rectangle.w = taille_carreau;
+                rectangle.h = 3*taille_carreau;
+                rectangle2.x = falling_meteor->x+1;
+                rectangle2.y = falling_meteor->y+2;
+                rectangle2.w = taille_carreau;
+                rectangle2.h = taille_carreau;
+            }
 
             // Affichage du rectangle
             SDL_RenderFillRect(*pRenderer, &rectangle);
 
-            
             break;
+
         case J:
             // Définition de la couleur du rectangle (bleu)
             SDL_SetRenderDrawColor(*pRenderer, 0, 0, 255, 255);
             
-            if (falling_meteor->rotation==0||falling_meteor->rotation==2){
-                rectangle = { falling_meteor->x-3*taille_carreau, 
-                                        falling_meteor->y-2*taille_carreau, 
-                                        3*taille_carreau, 2*taille_carreau };}
-            if (falling_meteor->rotation==1||falling_meteor->rotation==3){
-                rectangle = { falling_meteor->x-2*taille_carreau, 
-                                        falling_meteor->y-3*taille_carreau, 
-                                        3*taille_carreau, 2*taille_carreau };}
-
+            if (falling_meteor->rotation==0){
+                rectangle.x = falling_meteor->x;
+                rectangle.y = falling_meteor->y;
+                rectangle.w = 3*taille_carreau;
+                rectangle.h = taille_carreau;
+                rectangle2.x = falling_meteor->x+2;
+                rectangle2.y = falling_meteor->y+1;
+                rectangle2.w = taille_carreau;
+                rectangle2.h = taille_carreau;
+            }
+            if (falling_meteor->rotation==2){
+                rectangle.x = falling_meteor->x;
+                rectangle.y = falling_meteor->y+1;
+                rectangle.w = 3*taille_carreau;
+                rectangle.h = taille_carreau;
+                rectangle2.x = falling_meteor->x;
+                rectangle2.y = falling_meteor->y;
+                rectangle2.w = taille_carreau;
+                rectangle2.h = taille_carreau;
+            }
+            if (falling_meteor->rotation==1){
+                rectangle.x = falling_meteor->x+1;
+                rectangle.y = falling_meteor->y;
+                rectangle.w = taille_carreau;
+                rectangle.h = 3*taille_carreau;
+                rectangle2.x = falling_meteor->x;
+                rectangle2.y = falling_meteor->y+2;
+                rectangle2.w = taille_carreau;
+                rectangle2.h = taille_carreau;
+            }
+            if (falling_meteor->rotation==3){
+                rectangle.x = falling_meteor->x;
+                rectangle.y = falling_meteor->y;
+                rectangle.w = taille_carreau;
+                rectangle.h = 3*taille_carreau;
+                rectangle2.x = falling_meteor->x+1;
+                rectangle2.y = falling_meteor->y;
+                rectangle2.w = taille_carreau;
+                rectangle2.h = taille_carreau;
+            }
             // Affichage du rectangle
             SDL_RenderFillRect(*pRenderer, &rectangle);
 
             
             break;
-        case Z:
+/*        case Z:
             // Définition de la couleur du rectangle (rouge)
             SDL_SetRenderDrawColor(*pRenderer, 255, 0, 0, 255);
             
-            if (falling_meteor->rotation==0||falling_meteor->rotation==2){
-                rectangle = { falling_meteor->x-3*taille_carreau, 
-                                        falling_meteor->y-2*taille_carreau, 
-                                        3*taille_carreau, 2*taille_carreau };}
-            if (falling_meteor->rotation==1||falling_meteor->rotation==3){
-                rectangle = { falling_meteor->x-2*taille_carreau, 
-                                        falling_meteor->y-3*taille_carreau, 
-                                        3*taille_carreau, 2*taille_carreau };}
-
+            if (falling_meteor->rotation==0){
+                rectangle.x = falling_meteor->x;
+                rectangle.y = falling_meteor->y;
+                rectangle.w = 3*taille_carreau;
+                rectangle.h = taille_carreau;
+                rectangle2.x = falling_meteor->x;
+                rectangle2.y = falling_meteor->y+1;
+                rectangle2.w = taille_carreau;
+                rectangle2.h = taille_carreau;
+            }
+            if (falling_meteor->rotation==2){
+                rectangle.x = falling_meteor->x;
+                rectangle.y = falling_meteor->y+1;
+                rectangle.w = 3*taille_carreau;
+                rectangle.h = taille_carreau;
+                rectangle2.x = falling_meteor->x+2;
+                rectangle2.y = falling_meteor->y;
+                rectangle2.w = taille_carreau;
+                rectangle2.h = taille_carreau;
+            }
+            if (falling_meteor->rotation==1){
+                rectangle.x = falling_meteor->x+1;
+                rectangle.y = falling_meteor->y;
+                rectangle.w = taille_carreau;
+                rectangle.h = 3*taille_carreau;
+                rectangle2.x = falling_meteor->x;
+                rectangle2.y = falling_meteor->y;
+                rectangle2.w = taille_carreau;
+                rectangle2.h = taille_carreau;
+            }
+            if (falling_meteor->rotation==3){
+                rectangle.x = falling_meteor->x;
+                rectangle.y = falling_meteor->y;
+                rectangle.w = taille_carreau;
+                rectangle.h = 3*taille_carreau;
+                rectangle2.x = falling_meteor->x+1;
+                rectangle2.y = falling_meteor->y+2;
+                rectangle2.w = taille_carreau;
+                rectangle2.h = taille_carreau;
+            }
             // Affichage du rectangle
             SDL_RenderFillRect(*pRenderer, &rectangle);
 
@@ -133,5 +261,5 @@ void pre_render(SDL_Renderer** pRenderer, bloc * falling_meteor){
     }
 // Mise à jour du rendu
 SDL_RenderPresent(*pRenderer);
-
+*/
 }
