e3cac45a40d2f61f39912231b9f7daafbed22241
pailloncy <louispailloncy@MacBook-Pro-de-Louis-2.local>
2023-05-09 15:17:26 +0200

    bloc I

--- codes/pre_render.c
+++ codes/pre_render.c
@@ -2,45 +2,49 @@
 #include <SDL2/SDL.h>
 #include "def_types_et_vars.h"
 
-void pre_render(SDL_Renderer* pRenderer, bloc * falling_meteor){
+void pre_render(SDL_Renderer** pRenderer, bloc * falling_meteor){
     SDL_Rect rectangle;
     switch(falling_meteor->son_nom){
         case I :
             // Définition de la couleur du rectangle (turquoise)
-            SDL_SetRenderDrawColor(pRenderer, 255, 0, 255, 255);
+            SDL_SetRenderDrawColor(*pRenderer, 255, 0, 255, 255);
 
 
             if (falling_meteor->rotation==0||falling_meteor->rotation==2){
-                rectangle = { falling_meteor->x, 
-                                        falling_meteor->y, 
-                                        4*taille_carreau, taille_carreau };
+                rectangle.x = falling_meteor->x;
+                rectangle.y = falling_meteor->y;
+                rectangle.w = 4*taille_carreau;
+                rectangle.h = taille_carreau;
+
             }
             if (falling_meteor->rotation==1||falling_meteor->rotation==3){
-                rectangle = { falling_meteor->x, 
-                                        falling_meteor->y, 
-                                        taille_carreau, 4*taille_carreau };
+                rectangle.x = falling_meteor->x;
+                rectangle.y = falling_meteor->y;
+                rectangle.w = taille_carreau;
+                rectangle.h =4*taille_carreau;
+    
             }
 
             // Affichage du rectangle
-            SDL_RenderFillRect(pRenderer, &rectangle);
+            SDL_RenderFillRect(*pRenderer, &rectangle);
 
-            
             break;
+            
         case O:
             // Définition de la couleur du rectangle (jaune)
-            SDL_SetRenderDrawColor(pRenderer, 255, 255, 0, 255);
+            SDL_SetRenderDrawColor(*pRenderer, 255, 255, 0, 255);
 
             rectangle = { falling_meteor->x-2*taille_carreau, 
                                         falling_meteor->y-2*taille_carreau, 
                                         2*taille_carreau, 2*taille_carreau };
             // Affichage du rectangle
-            SDL_RenderFillRect(pRenderer, &rectangle);
+            SDL_RenderFillRect(*pRenderer, &rectangle);
 
             
             break;
         case T:
             // Définition de la couleur du rectangle (violet)
-            SDL_SetRenderDrawColor(pRenderer, 255, 0, 255, 255);
+            SDL_SetRenderDrawColor(*pRenderer, 255, 0, 255, 255);
             
             if (falling_meteor->rotation==0||falling_meteor->rotation==2){
                 rectangle = { falling_meteor->x-3*taille_carreau, 
@@ -51,12 +55,12 @@ void pre_render(SDL_Renderer* pRenderer, bloc * falling_meteor){
                                         falling_meteor->y-3*taille_carreau, 
                                         3*taille_carreau, 2*taille_carreau };}
             // Affichage du rectangle
-            SDL_RenderFillRect(pRenderer, &rectangle);
+            SDL_RenderFillRect(*pRenderer, &rectangle);
 
             break;
         case L:
              // Définition de la couleur du rectangle (orange)
-            SDL_SetRenderDrawColor(pRenderer, 255, 128, 0, 255);
+            SDL_SetRenderDrawColor(*pRenderer, 255, 128, 0, 255);
             
             if (falling_meteor->rotation==0||falling_meteor->rotation==2){
                 rectangle = { falling_meteor->x-3*taille_carreau, 
@@ -68,13 +72,13 @@ void pre_render(SDL_Renderer* pRenderer, bloc * falling_meteor){
                                         3*taille_carreau, 2*taille_carreau };}
 
             // Affichage du rectangle
-            SDL_RenderFillRect(pRenderer, &rectangle);
+            SDL_RenderFillRect(*pRenderer, &rectangle);
 
             
             break;
         case J:
             // Définition de la couleur du rectangle (bleu)
-            SDL_SetRenderDrawColor(pRenderer, 0, 0, 255, 255);
+            SDL_SetRenderDrawColor(*pRenderer, 0, 0, 255, 255);
             
             if (falling_meteor->rotation==0||falling_meteor->rotation==2){
                 rectangle = { falling_meteor->x-3*taille_carreau, 
@@ -86,13 +90,13 @@ void pre_render(SDL_Renderer* pRenderer, bloc * falling_meteor){
                                         3*taille_carreau, 2*taille_carreau };}
 
             // Affichage du rectangle
-            SDL_RenderFillRect(pRenderer, &rectangle);
+            SDL_RenderFillRect(*pRenderer, &rectangle);
 
             
             break;
         case Z:
             // Définition de la couleur du rectangle (rouge)
-            SDL_SetRenderDrawColor(pRenderer, 255, 0, 0, 255);
+            SDL_SetRenderDrawColor(*pRenderer, 255, 0, 0, 255);
             
             if (falling_meteor->rotation==0||falling_meteor->rotation==2){
                 rectangle = { falling_meteor->x-3*taille_carreau, 
@@ -104,13 +108,13 @@ void pre_render(SDL_Renderer* pRenderer, bloc * falling_meteor){
                                         3*taille_carreau, 2*taille_carreau };}
 
             // Affichage du rectangle
-            SDL_RenderFillRect(pRenderer, &rectangle);
+            SDL_RenderFillRect(*pRenderer, &rectangle);
 
             
             break;
         default : //case S
             // Définition de la couleur du rectangle (vert)
-            SDL_SetRenderDrawColor(pRenderer, 0, 255, 0, 255);
+            SDL_SetRenderDrawColor(*pRenderer, 0, 255, 0, 255);
             
             if (falling_meteor->rotation==0||falling_meteor->rotation==2){
                 rectangle = { falling_meteor->x-3*taille_carreau, 
@@ -122,12 +126,12 @@ void pre_render(SDL_Renderer* pRenderer, bloc * falling_meteor){
                                         3*taille_carreau, 2*taille_carreau };}
 
             // Affichage du rectangle
-            SDL_RenderFillRect(pRenderer, &rectangle);
+            SDL_RenderFillRect(*pRenderer, &rectangle);
 
             break;
         
     }
 // Mise à jour du rendu
-SDL_RenderPresent(pRenderer);
+SDL_RenderPresent(*pRenderer);
 
 }
