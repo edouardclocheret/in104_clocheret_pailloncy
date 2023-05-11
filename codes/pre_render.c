53f110a43aad873a002e99d07b95d034a6379e0a
pailloncy <louispailloncy@MacBook-Pro-de-Louis-2.local>
2023-05-11 17:58:36 +0200

    correction dans le dessin des rectangles

--- codes/pre_render.c
+++ codes/pre_render.c
@@ -58,28 +58,28 @@ void pre_render(SDL_Renderer** pRenderer, bloc * falling_meteor){
                 rectangle.y = falling_meteor->y;
                 rectangle.w = 3*taille_carreau;
                 rectangle.h = taille_carreau;
-                rectangle2.x = falling_meteor->x+1;
-                rectangle2.y = falling_meteor->y+1;
+                rectangle2.x = falling_meteor->x+taille_carreau;
+                rectangle2.y = falling_meteor->y+taille_carreau;
                 rectangle2.w = taille_carreau;
                 rectangle2.h = taille_carreau;
             }
             if (falling_meteor->rotation==2){
                 rectangle.x = falling_meteor->x;
-                rectangle.y = falling_meteor->y+1;
+                rectangle.y = falling_meteor->y+taille_carreau;
                 rectangle.w = 3*taille_carreau;
                 rectangle.h = taille_carreau;
-                rectangle2.x = falling_meteor->x+1;
+                rectangle2.x = falling_meteor->x+taille_carreau;
                 rectangle2.y = falling_meteor->y;
                 rectangle2.w = taille_carreau;
                 rectangle2.h = taille_carreau;
             }
             if (falling_meteor->rotation==1){
-                rectangle.x = falling_meteor->x+1;
+                rectangle.x = falling_meteor->x+taille_carreau;
                 rectangle.y = falling_meteor->y;
                 rectangle.w = taille_carreau;
                 rectangle.h = 3*taille_carreau;
                 rectangle2.x = falling_meteor->x;
-                rectangle2.y = falling_meteor->y+1;
+                rectangle2.y = falling_meteor->y+taille_carreau;
                 rectangle2.w = taille_carreau;
                 rectangle2.h = taille_carreau;
             }
@@ -88,8 +88,8 @@ void pre_render(SDL_Renderer** pRenderer, bloc * falling_meteor){
                 rectangle.y = falling_meteor->y;
                 rectangle.w = taille_carreau;
                 rectangle.h = 3*taille_carreau;
-                rectangle2.x = falling_meteor->x+1;
-                rectangle2.y = falling_meteor->y+1;
+                rectangle2.x = falling_meteor->x+taille_carreau;
+                rectangle2.y = falling_meteor->y+taille_carreau;
                 rectangle2.w = taille_carreau;
                 rectangle2.h = taille_carreau;
             }
@@ -110,22 +110,22 @@ void pre_render(SDL_Renderer** pRenderer, bloc * falling_meteor){
                 rectangle.w = 3*taille_carreau;
                 rectangle.h = taille_carreau;
                 rectangle2.x = falling_meteor->x;
-                rectangle2.y = falling_meteor->y+1;
+                rectangle2.y = falling_meteor->y+taille_carreau;
                 rectangle2.w = taille_carreau;
                 rectangle2.h = taille_carreau;
             }
             if (falling_meteor->rotation==2){
                 rectangle.x = falling_meteor->x;
-                rectangle.y = falling_meteor->y+1;
+                rectangle.y = falling_meteor->y+taille_carreau;
                 rectangle.w = 3*taille_carreau;
                 rectangle.h = taille_carreau;
-                rectangle2.x = falling_meteor->x+2;
+                rectangle2.x = falling_meteor->x+2*taille_carreau;
                 rectangle2.y = falling_meteor->y;
                 rectangle2.w = taille_carreau;
                 rectangle2.h = taille_carreau;
             }
             if (falling_meteor->rotation==1){
-                rectangle.x = falling_meteor->x+1;
+                rectangle.x = falling_meteor->x+taille_carreau;
                 rectangle.y = falling_meteor->y;
                 rectangle.w = taille_carreau;
                 rectangle.h = 3*taille_carreau;
@@ -139,8 +139,8 @@ void pre_render(SDL_Renderer** pRenderer, bloc * falling_meteor){
                 rectangle.y = falling_meteor->y;
                 rectangle.w = taille_carreau;
                 rectangle.h = 3*taille_carreau;
-                rectangle2.x = falling_meteor->x+1;
-                rectangle2.y = falling_meteor->y+2;
+                rectangle2.x = falling_meteor->x+taille_carreau;
+                rectangle2.y = falling_meteor->y+2*taille_carreau;
                 rectangle2.w = taille_carreau;
                 rectangle2.h = taille_carreau;
             }
@@ -160,14 +160,14 @@ void pre_render(SDL_Renderer** pRenderer, bloc * falling_meteor){
                 rectangle.y = falling_meteor->y;
                 rectangle.w = 3*taille_carreau;
                 rectangle.h = taille_carreau;
-                rectangle2.x = falling_meteor->x+2;
-                rectangle2.y = falling_meteor->y+1;
+                rectangle2.x = falling_meteor->x+2*taille_carreau;
+                rectangle2.y = falling_meteor->y+taille_carreau;
                 rectangle2.w = taille_carreau;
                 rectangle2.h = taille_carreau;
             }
             if (falling_meteor->rotation==2){
                 rectangle.x = falling_meteor->x;
-                rectangle.y = falling_meteor->y+1;
+                rectangle.y = falling_meteor->y+taille_carreau;
                 rectangle.w = 3*taille_carreau;
                 rectangle.h = taille_carreau;
                 rectangle2.x = falling_meteor->x;
@@ -176,12 +176,12 @@ void pre_render(SDL_Renderer** pRenderer, bloc * falling_meteor){
                 rectangle2.h = taille_carreau;
             }
             if (falling_meteor->rotation==1){
-                rectangle.x = falling_meteor->x+1;
+                rectangle.x = falling_meteor->x+taille_carreau;
                 rectangle.y = falling_meteor->y;
                 rectangle.w = taille_carreau;
                 rectangle.h = 3*taille_carreau;
                 rectangle2.x = falling_meteor->x;
-                rectangle2.y = falling_meteor->y+2;
+                rectangle2.y = falling_meteor->y+2*taille_carreau;
                 rectangle2.w = taille_carreau;
                 rectangle2.h = taille_carreau;
             }
@@ -190,7 +190,7 @@ void pre_render(SDL_Renderer** pRenderer, bloc * falling_meteor){
                 rectangle.y = falling_meteor->y;
                 rectangle.w = taille_carreau;
                 rectangle.h = 3*taille_carreau;
-                rectangle2.x = falling_meteor->x+1;
+                rectangle2.x = falling_meteor->x+taille_carreau;
                 rectangle2.y = falling_meteor->y;
                 rectangle2.w = taille_carreau;
                 rectangle2.h = taille_carreau;
@@ -210,18 +210,18 @@ void pre_render(SDL_Renderer** pRenderer, bloc * falling_meteor){
                 rectangle.y = falling_meteor->y;
                 rectangle.w = 2*taille_carreau;
                 rectangle.h = taille_carreau;
-                rectangle2.x = falling_meteor->x+1;
-                rectangle2.y = falling_meteor->y+1;
+                rectangle2.x = falling_meteor->x+taille_carreau;
+                rectangle2.y = falling_meteor->y+taille_carreau;
                 rectangle2.w = 2*taille_carreau;
                 rectangle2.h = taille_carreau;
             }
 
             if (falling_meteor->rotation==1||falling_meteor->rotation==3){
                 rectangle.x = falling_meteor->x;
-                rectangle.y = falling_meteor->y+1;
+                rectangle.y = falling_meteor->y+taille_carreau;
                 rectangle.w = taille_carreau;
                 rectangle.h = 2*taille_carreau;
-                rectangle2.x = falling_meteor->x+1;
+                rectangle2.x = falling_meteor->x+taille_carreau;
                 rectangle2.y = falling_meteor->y;
                 rectangle2.w = taille_carreau;
                 rectangle2.h = 2*taille_carreau;
@@ -237,12 +237,12 @@ void pre_render(SDL_Renderer** pRenderer, bloc * falling_meteor){
             SDL_SetRenderDrawColor(*pRenderer, 0, 255, 0, 255);
             
             if (falling_meteor->rotation==0||falling_meteor->rotation==2){
-                rectangle.x = falling_meteor->x+1;
+                rectangle.x = falling_meteor->x+taille_carreau;
                 rectangle.y = falling_meteor->y;
                 rectangle.w = 2*taille_carreau;
                 rectangle.h = taille_carreau;
                 rectangle2.x = falling_meteor->x;
-                rectangle2.y = falling_meteor->y+1;
+                rectangle2.y = falling_meteor->y+taille_carreau;
                 rectangle2.w = 2*taille_carreau;
                 rectangle2.h = taille_carreau;
             }
@@ -251,8 +251,8 @@ void pre_render(SDL_Renderer** pRenderer, bloc * falling_meteor){
                 rectangle.y = falling_meteor->y;
                 rectangle.w = taille_carreau;
                 rectangle.h = 2*taille_carreau;
-                rectangle2.x = falling_meteor->x+1;
-                rectangle2.y = falling_meteor->y+1;
+                rectangle2.x = falling_meteor->x+taille_carreau;
+                rectangle2.y = falling_meteor->y+taille_carreau;
                 rectangle2.w = taille_carreau;
                 rectangle2.h = 2*taille_carreau;
             }
