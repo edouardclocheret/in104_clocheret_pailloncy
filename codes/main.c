c4fb65f9f2fad70d84abf23d6274fead3bbafa78
edouardclocheret <edouard.clocheret@ensta-paris.fr>
2023-05-12 02:39:37 +0200

    création de la matrice qui représente les cases occupées et la fonction collision

--- codes/main.c
+++ codes/main.c
@@ -3,12 +3,50 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+#include "def_types_et_vars.h"
 #include "down.h"
 #include "entree_clavier.h"
 #include "initialisation.h"
 #include "rotation.h"
 #include "pre_render.h"
 
+
+int** create_matrix (int lignes, int colonnes){
+
+    // Allocation dynamique de la mémoire pour la matrice
+    int** matrice = (int**)malloc(lignes * sizeof(int*));
+    for (int i = 0; i < lignes; i++) {
+        matrice[i] = (int*)malloc(colonnes * sizeof(int));
+    }
+
+    // Initialisation de la matrice avec des zéros partout
+    for (int i = 0; i < lignes; i++) {
+        for (int j = 0; j < colonnes; j++) {
+            matrice[i][j] = 0;
+        }
+    }
+    return matrice;
+}
+
+bool collision(*bloc falling_meteor, int** matrice){
+    //traduire le bloc en ses 4 coordonnées des cases occupées
+    //arrondies à la case du dessus (si le bloc occupe que la moitié de la case, il n'y a pas encore collision)
+    int x1; int y1;
+    int x2; int y2;
+    int x3; int y3;
+    int x4; int y4;
+
+    //test de collision : 
+    if(y1==19||y2==19||y3==19||y4=19){
+        //modifier en plus la matrice en ajoutant la couleur
+        return true;
+    }
+    else if (matrice[x1][y1+1]!=0 ||matrice[x2][y2+1]!=0||matrice[x3][y3+1]!=0||matrice[x4][y4+1]!=0){
+        //modifier en plus la matrice en ajoutant la couleur
+        return true;
+    }
+    else return false;
+}
 
 //the falling bloc will be labeled "falling_meteor"
 
@@ -70,6 +108,13 @@ int main(int argc, char *argv[]){
     printf("11\n");
     SDL_Delay(20000);
     
+    //cette matrice représente les blocs déjà tombés
+    int** matrice = create_matrix(20,10);
+    printf("Bloc en psoition 1,3%d\n", matrice[1][3]);
+
+    //if collision(*bloc falling_meteor, int** matrice);
+    
+    
     /*On mettra en place la boucle de jeu plus tard
     //début du jeu
 
@@ -99,6 +144,13 @@ int main(int argc, char *argv[]){
     if(NULL != window) SDL_DestroyWindow(window);
 
     SDL_Quit();
+
+
+    // Libération de la mémoire allouée pour la matrice
+    for (int i = 0; i < 20; i++) {
+        free(matrice[i]);
+    }
+    free(matrice);
 
     return EXIT_SUCCESS;
 }
